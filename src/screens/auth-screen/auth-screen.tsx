3d9d85275bfa960c49d0e0b0212c54e044be0e27
suddenlyGiovanni <ravalico.giovanni@gmail.com>
2019-02-08 14:37:49 +0100

    added styled-components

--- src/screens/auth-screen/auth-screen.tsx
+++ src/screens/auth-screen/auth-screen.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Button, Text, View } from 'react-native';
+import styled from "styled-components/native";
 
 interface Props {
   readonly componentId: string;
@@ -8,16 +9,26 @@ interface Props {
 interface State {
   readonly value: number;
 }
+
+const Wrapper = styled(View)`
+  width: 100%;
+  flex: 1;
+  background-color: papayawhip;
+`;
+
+const Heading = styled(Text)`
+  color: palevioletred;
+`;
 class AuthScreen extends React.PureComponent<Props, State> {
   readonly state: State = { value: 0 };
 
   render(): JSX.Element {
     return (
-      <View>
-        <Text>Auth Screen</Text>
+      <Wrapper>
+        <Heading>Auth Screen</Heading>
         <Button title="increment" onPress={this.loginHandler} />
         <Text>{this.state.value}</Text>
-      </View>
+      </Wrapper>
     );
   }
   private readonly loginHandler = (e: any): void => {
