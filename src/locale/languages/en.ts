828ec958f2a759870fd48bc4c1bc7b22945d83a2
suddenlyGiovanni <ravalico.giovanni@gmail.com>
2019-02-25 17:35:11 +0100

    [WIP] auth screen

--- src/locale/languages/en.ts
+++ src/locale/languages/en.ts
@@ -1,5 +1,5 @@
-// tslint:disable no-object-mutation
-export const en = {
+// tslint:disable no-object-mutation no-hardcoded-credentials
+const en = {
   title: 'example',
   platform: {
     ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
@@ -8,6 +8,17 @@ export const en = {
   },
   greetings: 'Welcome to React Native!',
   instructions: 'To get started, edit app.tsx',
+  login: {
+    title: 'LOGIN',
+    withFacebook: 'Sign in with Facebook',
+    withGoogle: 'Sign in with Google',
+    dontHaveAnAccount: "Don't have an account yet?",
+    signUpNow: 'Sign up now',
+    or: 'or',
+    passwordForgotten: 'Password forgotten?',
+    placeholderEmail: 'Email Address',
+    placeholderPassword: 'Password',
+  },
 };
 
 // has to export as commonJs to allow the `i18n-dts` script to auto generate the typings for us.
--- types/i18n-js.d.ts
+++ types/i18n-js.d.ts
@@ -1,7 +1,7 @@
 declare module 'i18n-js' {
   var fallbacks: boolean;
   var translations: {
-    readonly [keys: string]: any;
+    [keys: string]: any;
   };
   var defaultLocale: string;
   var locale: string;
@@ -15,6 +15,15 @@ declare module 'i18n-js' {
   ): 'Double tap R on your keyboard to reload,\nShake or press menu button for dev menu';
   function t(key: 'greetings'): 'Welcome to React Native!';
   function t(key: 'instructions'): 'To get started, edit app.tsx';
+  function t(key: 'login.title'): 'LOGIN';
+  function t(key: 'login.withFacebook'): 'Sign in with Facebook';
+  function t(key: 'login.withGoogle'): 'Sign in with Google';
+  function t(key: 'login.dontHaveAnAccount'): "Don't have an account yet?";
+  function t(key: 'login.signUpNow'): 'Sign up now';
+  function t(key: 'login.or'): 'or';
+  function t(key: 'login.passwordForgotten'): 'Password forgotten?';
+  function t(key: 'login.placeholderEmail'): 'Email Address';
+  function t(key: 'login.placeholderPassword'): 'Password';
 }
 
 declare module '*.json' {
