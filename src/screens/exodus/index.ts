b09472952060c6e0ad292bdf93a14e3d5261e229
suddenlyGiovanni <ravalico.giovanni@gmail.com>
2019-02-28 11:12:35 +0100

    [fix] - sign up and sign in

---
+++ src/screens/exodus/index.ts
@@ -0,0 +1,4 @@
+export { default as SignInScreen } from './auth/sign-in.screen';
+export { default as SignUpScreen } from './auth/sign-up.screen';
+export { default as HomeScreen } from './home/home.screen';
+export { default as Initialize } from './initialize/initialize.screen';
